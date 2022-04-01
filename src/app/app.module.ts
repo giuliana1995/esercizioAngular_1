24370782fde868d66fdadc11f650a459c6d8b5e0
Giuliana dev <GiulianaDev@Air-di-Giuliana.station>
2022-04-01 14:41:47 +0200

    firts-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OrderContainerComponent } from './order-container/order-container.component';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { OrderSingleComponent } from './order-single/order-single.component';
+import { UserContainerComponent } from './user-container/user-container.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserSingleComponent } from './user-single/user-single.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderContainerComponent,
+    OrderDetailComponent,
+    OrderListComponent,
+    OrderSingleComponent,
+    UserContainerComponent,
+    UserDetailComponent,
+    UserListComponent,
+    UserSingleComponent
   ],
   imports: [
     BrowserModule
